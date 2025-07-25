bc7256f0d6680df9680de370d251d3cdc2b81476
officialryder1~ <kennethvic07@gmail.com>
2025-07-25 14:38:32 +0100

    skill acquisition

---
+++ src/lib/components/Skill.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { ConstructionIcon } from "@lucide/svelte";
+
+</script>
+
+<div class="min-h-screen flex flex-col items-center justify-center bg-base-300 text-gray-100 p-6">
+    <ConstructionIcon size=60 color="red" class="animate-bounce"/>
+    <h1 class="font-extrabold text-5xl pb-5">Coming soon!</h1>
+    <p class="font-mono mb-4">Skill acquisition functionality is under development. Please check back later.</p>
+    <a href="/" class="btn bg-green-500 hover:bg-green-600 text-white">Go to Home</a>
+</div>
