b893636e3e2f5f9c7b1b14aa1be4989c90981579
officialryder1~ <kennethvic07@gmail.com>
2025-08-01 19:37:39 +0100

    fix error again

--- src/routes/(landing_page)/post-a-job/+page.svelte
+++ src/routes/(landing_page)/post-a-job/+page.svelte
@@ -1,4 +1,4 @@
-<div class="min-h-screen flex flex-col items-center justify-center bg-base-300 text-gray-100 p-6">
+<div class="min-h-screen flex flex-col items-center justify-center bg-base-300 p-6">
     <ConstructionIcon size=60 color="red" class="animate-bounce"/>
     <h1 class="font-extrabold text-5xl pb-5">Coming soon!</h1>
     <p class="font-mono mb-4">post a job functionality is under development. Please check back later.</p>
