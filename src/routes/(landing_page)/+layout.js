b5a79d258b2b47233ca8d4af7170791abce6b0a3
officialryder1~ <kennethvic07@gmail.com>
2025-08-06 15:08:57 +0100

    new blog post

--- src/routes/(landing_page)/+layout.js
+++ src/routes/(landing_page)/+layout.js
@@ -3,18 +3,24 @@ export const prerender = true;
 export async function load() {
     const posts = import.meta.glob('$lib/post/*.svx', { eager: true });
 
-    return {
-        posts: Object.entries(posts).map(([path, post]) => {
-            const slug = path
-                .split('/')
-                .pop()
-                .replace('.svx', '');
+    const allPosts = Object.entries(posts).map(([path, post]) => {
+        const slug = path
+            .split('/')
+            .pop()
+            .replace('.svx', '');
 
-            return {
-                ...post.metadata,
-                slug,
-                path: `/blog/${slug}`
-            };
-        })
+        return {
+            ...post.metadata,
+            path: `/blog/${slug}`
+        };
+    });
+
+    // Sort by date descending
+    allPosts.sort((a, b) => {
+        return new Date(b.date).getTime() - new Date(a.date).getTime();
+    });
+
+    return {
+        posts: allPosts.slice(0, 4)
     };
 }
